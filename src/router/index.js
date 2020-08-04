fdb225904c812c60e8901640d838ec9372bc776e
Bepal.er <348118672@qq.com>
2020-08-04 11:26:51 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '@/views/Login.vue'
-import { token } from '@config'
 
 Vue.use(VueRouter)
 
@@ -18,14 +16,6 @@ const routes = [
       title: '主页'
     },
     component: Home
-  },
-  {
-    path: '/login',
-    name: 'login',
-    meta: {
-      title: '登录'
-    },
-    component: Login
   }
 ]
 
@@ -35,14 +25,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if (to && to.meta && to.meta.title) {
-    document.title = to.meta.title
-  }
-  if (to.path !== '/login' && !Vue.ls.get(`${token}`)) {
-    next({ replace: true, name: 'login', query: { redirect: to.path } })
-  } else {
-    next()
-  }
+  next()
 })
 
 export default router
