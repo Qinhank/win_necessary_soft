a88161bd1fec071cfd3d6907cbc8f7ee939a29d8
Bepal.er <348118672@qq.com>
2020-08-04 12:07:33 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,27 @@
 
 const { proxy, port, alias, title, useCDN, cdn } = require('./static/config')
+const path = require('path')
+const fs = require('fs')
 const { DynamicImportCdnPlugin } = require('webpack-dynamic-import-cdn-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+// 将view文件下的子文件遍历并生成routers
+const loopViews = (name) => {
+  const directory = `./src/${name || 'views'}`
+  const arr = fs.readdirSync(directory)
+  return arr.map(_ => {
+    return `/${ _.replace('.vue', '') }`
+  })
+}
 
 module.exports = {
-  outputDir: 'static/public',
+  outputDir: 'gh-pages',
   publicPath: './',
   configureWebpack: { // webpack配置，值位对象时会合并配置，为方法时会改写配置
     resolve: {
       alias
     },
-    plugins: useCDN ? [new DynamicImportCdnPlugin({
+    plugins: [...useCDN ? [new DynamicImportCdnPlugin({
       css: {
         'element-ui/lib/theme-chalk/index.css':
           `${cdn}/npm/element.min.css`
@@ -24,7 +36,12 @@ module.exports = {
           url: `${cdn}/npm/element.min.js`
         }
       }
-    })] : []
+    })] : [], new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'gh-pages'),
+      // Required - Routes to render.
+      routes: [ '/', ...loopViews() ]
+    }) ]
   },
   chainWebpack: config => {
     config
