364bb297fce50afc213f453b0749b7d1df2e7508
覃凯旋 <hank.qin@fowo.com>
2021-10-29 16:21:33 +0800

    feat: 更新一波新软件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,84 @@
 <style lang="scss">
-  #home{
-    position: relative;
-    z-index: 99;
-    .el-tabs{
-      width: calc(100% - 60px);
-      margin: 10px auto 0 auto;
-    }
-    h2{
-      font-size: 24px;
-      margin-bottom: 20px;
-      font-weight: bold;
-    }
-    .top{
-      height: 150px;
-      margin: 10px 30px 15px 30px;
-      width: calc(100% - 60px);
-      &-box{
-        display: flex;
-        align-items: center;
-        .top-left{
-          width: 50px;
-          text-align: right;
-          margin-right: 20px;
-          i{
-            font-size: 26px;
-            color: $green;
-          }
-        }
-        .top-center{
-          width: calc(100% - 200px);
-          h3{
-            font-size: 26px;
-            font-weight: bold;
-          }
-          p{
-            font-size: 14px;
-            color: $grey;
-          }
+#home {
+  position: relative;
+  z-index: 99;
+  .el-tabs {
+    width: calc(100% - 60px);
+    margin: 10px auto 0 auto;
+  }
+  h2 {
+    font-size: 24px;
+    margin-bottom: 20px;
+    font-weight: bold;
+  }
+  .top {
+    height: 150px;
+    margin: 10px 30px 15px 30px;
+    width: calc(100% - 60px);
+    &-box {
+      display: flex;
+      align-items: center;
+      .top-left {
+        width: 50px;
+        text-align: right;
+        margin-right: 20px;
+        i {
+          font-size: 26px;
+          color: $green;
         }
       }
-    }
-    .bottom{
-      height: calc(100vh - 240px);
-      width: calc(100% - 60px);
-      margin: 0px 30px 0 30px;
-      .head{
-        display: flex;
-        align-items: center;
-        padding-left: 20px;
-        border-bottom: 1px solid #eee;
-        padding-bottom: 15px;
-        margin-bottom: 10px;
-        &-item{
-          width: 25%;
-          text-align: left;
+      .top-center {
+        width: calc(100% - 200px);
+        h3 {
+          font-size: 26px;
           font-weight: bold;
         }
-      }
-      .content{
-        padding-left: 20px;
-        &-item{
-          display: flex;
-          align-items: center;
-          color: rgba(0,0,0,.7);
-          margin-bottom: 15px;
-          &-text{
-            width: 25%;
-          }
+        p {
+          font-size: 14px;
+          color: $grey;
         }
       }
     }
   }
+  .bottom {
+    height: calc(100vh - 240px);
+    width: calc(100% - 60px);
+    margin: 0px 30px 0 30px;
+    .head {
+      display: flex;
+      align-items: center;
+      padding-left: 20px;
+      border-bottom: 1px solid #eee;
+      padding-bottom: 15px;
+      margin-bottom: 10px;
+      &-item {
+        width: 25%;
+        text-align: left;
+        font-weight: bold;
+      }
+    }
+    .content {
+      padding-left: 20px;
+      &-item {
+        display: flex;
+        align-items: center;
+        color: rgba(0, 0, 0, 0.7);
+        margin-bottom: 15px;
+        &-text {
+          width: 25%;
+        }
+      }
+    }
+  }
+}
 </style>
 
 <template>
   <div id="home">
-    <t-card class="top" card-class="top-box">
+    <t-card
+      class="top"
+      card-class="top-box"
+    >
       <div class="top-left">
         <i class="el-icon-success"></i>
       </div>
@@ -85,7 +88,12 @@
       </div>
     </t-card>
     <el-tabs v-model="activeName">
-      <el-tab-pane :label="item.name" :name="`${index}`" v-for="(item, index) in data" :key="index"></el-tab-pane>
+      <el-tab-pane
+        :label="item.name"
+        :name="`${index}`"
+        v-for="(item, index) in data"
+        :key="index"
+      ></el-tab-pane>
     </el-tabs>
     <t-card class="bottom">
       <div class="head">
@@ -95,13 +103,27 @@
         <div class="head-item">操作</div>
       </div>
       <div class="content">
-        <div class="content-item" v-for="(item, index) in data[activeName-0].softs" :key="index">
+        <div
+          class="content-item"
+          v-for="(item, index) in data[activeName-0].softs"
+          :key="index"
+        >
           <div class="content-item-text">{{ item.name }}</div>
-          <div class="content-item-text" style="padding-right: 20px">{{ item.des }}</div>
+          <div
+            class="content-item-text"
+            style="padding-right: 20px"
+          >{{ item.des }}</div>
           <div class="content-item-text">{{ item.version }}</div>
           <div class="content-item-btn">
-            <el-button type="primary" @click="go2(item.down)" :disabled="!item.down">{{ item.down ? '立即下载' : '暂无下载' }}</el-button>
-            <el-button @click="go2(item.link)">访问官方</el-button>
+            <el-button
+              type="primary"
+              @click="go2(item.down)"
+              :disabled="!item.down"
+            >{{ item.down ? '立即下载' : '暂无下载' }}</el-button>
+            <el-button
+              @click="go2(item.link)"
+              :disabled="!item.link"
+            >{{item.link ? '访问官方' : '暂无官网'}}</el-button>
           </div>
         </div>
       </div>
